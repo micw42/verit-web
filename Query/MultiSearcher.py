021844ef773f1c23b79868353145b96c0f00d210
elu2 <60558250+elu2@users.noreply.github.com>
2022-10-24 12:24:12 -0700

    Regex false for improved speed

--- Query/MultiSearcher.py
+++ Query/MultiSearcher.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 def name_query(name, nodes, full_df, string_type):
     if string_type == "starts_with":
-        name_query = full_df.query('name.str.startswith(@name, na=False)', engine='python').reset_index(drop=True)
+        name_query = full_df.query('name.str.startswith(@name, na=False, regex=False)', engine='python').reset_index(drop=True)
     
     elif string_type == "ends_with":
-        name_query = full_df.query('name.str.endswith(@name, na=False)', engine='python').reset_index(drop=True)
+        name_query = full_df.query('name.str.endswith(@name, na=False, regex=False)', engine='python').reset_index(drop=True)
     
     elif string_type == "contains":
-        name_query = full_df.query('name.str.contains(@name, na=False)', engine='python').reset_index(drop=True)
+        name_query = full_df.query('name.str.contains(@name, na=False, regex=False)', engine='python').reset_index(drop=True)
         
 
     # The following nodes are in the network, but REACH likely grounds to more
