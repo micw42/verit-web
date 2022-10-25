624bf4eb9ead73592f97874dd88c48a267e6703c
elu2 <60558250+elu2@users.noreply.github.com>
2022-10-25 13:20:09 -0700

    Implemented direct gene querying
    Requires that uniprot_df is loaded globally. New pickle file.

--- Query/MultiSearcher.py
+++ Query/MultiSearcher.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 
 def name_query(name, nodes, full_df, string_type):
     if string_type == "starts_with":
@@ -8,16 +10,15 @@ def name_query(name, nodes, full_df, string_type):
         name_query = full_df.query('name.str.endswith(@name, na=False, regex=False)', engine='python').reset_index(drop=True)
     
     elif string_type == "contains":
-        name_query = full_df.query('name.str.contains(@name, na=False, regex=False)', engine='python').reset_index(drop=True)
-        
+        name_query = full_df.query('name.str.contains(@name, na=False, regex=False)', engine='python').reset_index(drop=True)        
 
     # The following nodes are in the network, but REACH likely grounds to more
     in_net = pd.merge(nodes, name_query, left_on="Id", right_on="id", how="inner").iloc[:, 3:6].drop_duplicates().reset_index(drop=True)
-    
+
     # Reverse search to collect all aliases
     u_ids = list(in_net["id"].drop_duplicates())
     in_net = full_df.query("id == @u_ids").iloc[::-1].reset_index(drop=True)
-    
+
     in_net["user_query"] = name
     in_net = pd.merge(in_net, nodes, left_on="id", right_on = "Id", how="inner")
     in_net = in_net.drop_duplicates(subset = ["name", "id"])
@@ -26,19 +27,40 @@ def name_query(name, nodes, full_df, string_type):
 
 
 def multi_query(query_list, nodes, full_df, string_type):
-    # initialize empty dataframe
-    full_query = pd.DataFrame()
+    # Gene querying can be streamlined
+    if string_type == "gene":
+        # Get found and unfound queries
+        mapped = np.intersect1d(query_list, list(full_df["Label"]), return_indices=True)[2]
+        mapped_ids = full_df.iloc[mapped].drop_duplicates(subset="Id")
 
-    for query in query_list:
-        full_query = full_query.append(name_query(query, nodes, full_df, string_type))
+        unmapped = np.setdiff1d(query_list, list(full_df["Label"]))    # Unused
+
+        # Get only IDs and PR values
+        nodes_dd = nodes[["Id", "PR"]].drop_duplicates()
+
+        # Unique IDs within VERIT network
+        in_net = nodes_dd.merge(mapped_ids, on="Id", how="inner")
+
+        # Reconstruct expected format
+        in_net.columns = ["id", "PR", "name"]
+        in_net["id"] = in_net[["name", "id"]].agg("::".join, axis=1)
+        in_net["user_query"] = in_net["name"]
+        
+        return in_net
+
+    else:
+        # initialize empty dataframe
+        full_query = pd.DataFrame()
+        for query in query_list:
+            full_query = full_query.append(name_query(query, nodes, full_df, string_type))
 
     return full_query
-
 
 
 def query(query_list, nodes, full_df, string_type):
     # Turns all entries into lower case
     case_proof = [x.lower() for x in query_list]
-    multi_query(case_proof, nodes, full_df, string_type).to_csv("multiSearchOut.csv", index=False)
-
-
+    if string_type == "gene":
+        multi_query(case_proof, nodes, uniprot_df, string_type).to_csv("multiSearchOut.csv", index=False)
+    else:
+        multi_query(case_proof, nodes, full_df, string_type).to_csv("multiSearchOut.csv", index=False)
